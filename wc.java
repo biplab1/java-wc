abc10d2769a538730afd147d487289f9c162393e
Biplab Dutta <biplab1@users.noreply.github.com>
2024-01-31 18:24:42 +0530

    Update wc.java

--- wc.java
+++ wc.java
@@ -1,10 +1,26 @@
-// Goal of the project: wc – word, line, character, and byte count.
-// default case without any argument: -c, -l and -w (same order)
+/*
+MIT License
 
-// -l : no. of lines
-// -c : no. of bytes
-// -w : no. of words
-// -m : no. of characters
+Copyright (c) 2024 Biplab Dutta
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 import java.io.*;
 import java.util.*;
